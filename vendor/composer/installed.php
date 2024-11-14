4f3e88faa713a61f88c75788a61d712907fb6d12
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2024-11-14 14:14:56 +0700

    feat: setup folder directori and install jwt

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'php_basic/php_api',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'da3d797fd12cd5e8b2706ab5327e083d4c9080e5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v6.10.1',
+            'version' => '6.10.1.0',
+            'reference' => '500501c2ce893c824c801da135d02661199f60c5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'php_basic/php_api' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'da3d797fd12cd5e8b2706ab5327e083d4c9080e5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
