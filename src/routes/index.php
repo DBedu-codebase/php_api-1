9af77ab3cb51959ec316c9abc274b06b042219ac
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2025-01-24 15:04:33 +0700

    refactor: to config.php

--- src/routes/index.php
+++ src/routes/index.php
@@ -7,12 +7,12 @@ require_once __DIR__ . '/../utils/RoutingHelper.php';
 use App\Http\Router;
 use App\Controllers\BlogControllers;
 use App\Controllers\UserControllers; // No change needed here
-use App\Model\Database;
+use App\Config\Config;
 
 $input = json_decode(file_get_contents('php://input'), true);
 header("Content-Type: application/json");
 
-$PDO = new Database();
+$PDO = new Config();
 $router = new Router();
 $router->post('/api/v1/auth/register', fn() => UserControllers::register($PDO->getPdo()));
 $router->post('/api/v1/auth/login', fn() => UserControllers::login($PDO->getPdo()));
