aa1d898f7ad42857c99fa8fd6ea3dbee1f1d66ae
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2025-01-24 16:44:53 +0700

    feat: blog models querry

--- src/controllers/Blog/Blog.php
+++ src/controllers/Blog/Blog.php
@@ -2,36 +2,28 @@
 
 namespace App\Controllers\Blog;
 
+use App\Core\Validation;
 use App\Middleware\AuthMiddleware;
-
+use App\Model\BlogModel;
 
 class Blog
 {
-     public static function getAll($pdo)
+     public function getAll($pdo)
      {
-          $auth = new AuthMiddleware();
-          $auth->authenticate();
-          $sql = "SELECT * FROM blog";
-          $stmt = $pdo->prepare($sql);
-          $stmt->execute();
-          $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+          $model =  new BlogModel();
+          $model->getAll();
           echo json_encode([
                'message' => "Successfully Get All Blog",
                'data' => [
-                    'blog' => $result
+                    'blog' => $model
                ]
           ]);
      }
 
-     public static function getBlogById($pdo, $id)
+     public  function getBlogById($pdo, $id)
      {
-          $auth = new AuthMiddleware();
-          $auth->authenticate();
-          $sql = "SELECT * FROM blog WHERE post_id = :id";
-          $stmt = $pdo->prepare($sql);
-          $stmt->execute([':id' => $id]);
-          $result = $stmt->fetch(\PDO::FETCH_ASSOC);
-
+          $model =  new BlogModel();
+          $result = $model->getById($id);
           if (empty($result)) {
                http_response_code(404);
                echo json_encode([
@@ -49,32 +41,17 @@ class Blog
      }
      public static function delete($pdo, $id)
      {
-          $auth = new AuthMiddleware();
-          $auth->authenticate();
-          // Validate the ID
-          if (empty($id) || !is_numeric($id)) {
-               http_response_code(400);
-               echo json_encode(['error' => 'Invalid ID']);
-               exit();
-          }
-
-          // Check if the blog post exists
-          $checkSql = "SELECT * FROM blog WHERE post_id = :id";
-          $checkStmt = $pdo->prepare($checkSql);
-          $checkStmt->execute([':id' => $id]);
-          $result = $checkStmt->fetch(\PDO::FETCH_ASSOC);
-
-          if (!$result) {
+          $model =  new BlogModel();
+          $result =  $model->getById($id);
+          if (empty($result)) {
                http_response_code(404);
-               echo json_encode(['error' => 'Blog post not found']);
+               echo json_encode([
+                    'error' => 'Blog not found',
+               ]);
                exit();
           }
-
           // Proceed to delete the blog post
-          $sql = "DELETE FROM blog WHERE post_id = :id";
-          $stmt = $pdo->prepare($sql);
-          $stmt->execute([':id' => $id]);
-
+          $model->delete($id);
           echo json_encode(['message' => 'Deleted successfully']);
      }
 }
--- src/model/BlogModel.php
+++ src/model/BlogModel.php
@@ -1,1 +1,62 @@
 <?php
+
+namespace App\Model;
+
+use App\Config\Config;
+use PDO;
+
+class BlogModel extends Config
+{
+     private $table = 'blog';
+
+     public function getAll(): array
+     {
+          $PDO = $this->getPdo();
+          $sql = "SELECT * FROM {$this->table}";
+          $stmt = $PDO->prepare($sql);
+          $stmt->execute();
+
+          return $stmt->fetchAll(PDO::FETCH_ASSOC);
+     }
+
+     public function getById(int $id)
+     {
+          $PDO = $this->getPdo();
+          $sql = "SELECT * FROM {$this->table} WHERE post_id = :id";
+          $stmt = $PDO->prepare($sql);
+          $stmt->execute([':id' => $id]);
+
+          return $stmt->fetch(PDO::FETCH_ASSOC);
+     }
+
+     public function create(array $data): void
+     {
+          $PDO = $this->getPdo();
+          $sql = "INSERT INTO {$this->table} (title, content) VALUES (:title, :content)";
+          $stmt = $PDO->prepare($sql);
+          $stmt->execute([
+               ':title' => $data['title'],
+               ':content' => $data['content'],
+          ]);
+     }
+
+     public function update(int $id, array $data): void
+     {
+          $PDO = $this->getPdo();
+          $sql = "UPDATE {$this->table} SET title = :title, content = :content WHERE id = :id";
+          $stmt = $PDO->prepare($sql);
+          $stmt->execute([
+               ':id' => $id,
+               ':title' => $data['title'],
+               ':content' => $data['content'],
+          ]);
+     }
+
+     public function delete(int $id): void
+     {
+          $PDO = $this->getPdo();
+          $sql = "DELETE FROM {$this->table} WHERE post_id = :id";
+          $stmt = $PDO->prepare($sql);
+          $stmt->execute([':id' => $id]);
+     }
+}
